091b061db45443c91a987f749f1a94842eb9ed8c
321niloy <niloyn670@gmail.com>
2023-06-11 11:15:15 +0600

    get users,vercel.json

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 console.log(process.env.ADMIN_USER)
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 var uri = `mongodb://${process.env.ADMIN_USER}:${process.env.ADDMIN_PASS}@ac-mvazqsy-shard-00-00.hpy6sqt.mongodb.net:27017,ac-mvazqsy-shard-00-01.hpy6sqt.mongodb.net:27017,ac-mvazqsy-shard-00-02.hpy6sqt.mongodb.net:27017/?ssl=true&replicaSet=atlas-vll8ae-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,19 +28,77 @@ async function run() {
     await client.connect();
 
     const instructorsCollection = client.db("learnHub").collection("instructor");
-    const instruementCollection = client.db("learnHub").collection("instruement");
+    const instrumentCollection = client.db("learnHub").collection("instruement");
+    const usersCollection = client.db("learnHub").collection("users");
+    const getCollection=client.db("learnHub").collection("get");
     // Send a ping to confirm a successful connection
+    // for user
+    app.put('/users/:email', async(req,res)=>{
+        const email = req.params.email
+        const user = req.body
+        const query = {email:email}
+        const options = {upsert: true }
+        const updateDoc = {
+            $set:user
+        }
+        const result =await usersCollection.updateOne(query,updateDoc,options)
+        console.log(result)
+        res.send(result)
+    })
+    
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
+    // //////////////////////////////
+
+    // for instructos
 
     app.get('/instructor', async (req, res) => {
         const result = await instructorsCollection.find().toArray();
         res.send(result);
       });
+//  ------------------------------------
 
+// ------------Instrument
       app.get('/instruement', async (req, res) => {
-        const result = await instruementCollection.find().toArray();
+        const result = await instrumentCollection.find().toArray();
         res.send(result);
       });
+      
+      app.post('/instruement', async(req,res)=>{
+        const newItem = req.body;
+        console.log(newItem)
+        const result = await instrumentCollection.insertOne(newItem)
+        res.send(result);
+    });
+    //   ///////////////////////////////////////
+
+    // -----------------------------get instrument classed
+    app.post('/instrucarts', async(req,res)=>{
+        const newItem = req.body;
+        console.log(newItem)
+        const result = await getCollection.insertOne(newItem)
+        res.send(result);
+    })
+
+      app.get('/instrucarts/:email', async (req, res) => {
+        const email=req.params.email
+        const query = { email: email }
+        const result = await getCollection.find(query).toArray();
+        res.send(result);
+      });
+
+      app.delete('/instrucarts/:id',async (req, res) => {
+        const id = req.params.id;
+        console.log(id)
+        const query = { _id: new ObjectId(id) }
+        const result = await getCollection.deleteOne(query);
+        res.send(result);
+      })
+
+    // //////////////////////////////////////////////
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
